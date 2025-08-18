1d2215de657e46c4cc32dc08b353c8f31240b537
ebabar5 <ehsanbabar9@gmail.com>
2025-08-18 22:44:10 +0300

    render template

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ vectorizer = joblib.load("vectorizer.pkl")
 
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/predict',methods=['POST'])
 def predict():
     data= request.get_json() or {}
@@ -26,6 +27,6 @@ def predict():
     return jsonify({"label":str(prediction), "confidence": confidence})
     
 if __name__=='__main__':
-    app.run(debug=True,port=5000)
+    app.run()
 
 
